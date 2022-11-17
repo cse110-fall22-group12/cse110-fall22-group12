5c277b00c9fbbe5b63c001500daf7458cef9d3fd
Kelin Lyu <KelinLyuACWG@gmail.com>
2022-11-17 11:45:02 -0800

    Fix minor issues.

--- source/js/edit-page.js
+++ source/js/edit-page.js
@@ -3,29 +3,20 @@
  * is creating a new recipe or editing a current one.
  *
  */
-function construct_edit_page() {
+window.onload = function() {
+
   console.log('constructing the edit page...');
-}
 
-window.onload = construct_edit_page;
+  document.getElementById('edit-page-back-button').addEventListener('click', function() {
+    console.log('returning to the previous page...');
+  });
 
-/**
- * return to the previous page.
- *
- */
-function edit_page_back_event_handler() {
-  console.log('returning to the previous page...');
-}
-
-/**
- * save the current data by creating a new element
- * or overwrite the existing one.
- *
- */
-function edit_page_save_event_handler() {
-  console.log('saving the following data');
-  console.log(document.getElementById('edit-page-input-name').value);
-  console.log(document.getElementById('edit-page-input-ingredients').value);
-  console.log(document.getElementById('edit-page-input-steps').value);
-  console.log(document.getElementById('edit-page-input-notes').value);
-}
+  document.getElementById('edit-page-save-button').addEventListener('click', function() {
+    console.log('saving the following data');
+    console.log(document.getElementById('edit-page-input-name').value);
+    console.log(document.getElementById('edit-page-input-ingredients').value);
+    console.log(document.getElementById('edit-page-input-steps').value);
+    console.log(document.getElementById('edit-page-input-notes').value);
+  });
+  
+};
