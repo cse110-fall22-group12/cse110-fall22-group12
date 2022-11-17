109513a355e5963759a848eb633448dbe38fce4a
Kelin Lyu <KelinLyuACWG@gmail.com>
2022-11-17 11:47:56 -0800

    Fix minor issues.

--- source/js/edit-page.js
+++ source/js/edit-page.js
@@ -4,19 +4,20 @@
  *
  */
 window.onload = function() {
-
+  // initialize thepage here:
   console.log('constructing the edit page...');
-
-  document.getElementById('edit-page-back-button').addEventListener('click', function() {
+  // return:
+  const back = document.getElementById('edit-page-back-button');
+  back.addEventListener('click', function() {
     console.log('returning to the previous page...');
   });
-
-  document.getElementById('edit-page-save-button').addEventListener('click', function() {
+  // save:
+  const save = document.getElementById('edit-page-save-button');
+  save.addEventListener('click', function() {
     console.log('saving the following data');
     console.log(document.getElementById('edit-page-input-name').value);
     console.log(document.getElementById('edit-page-input-ingredients').value);
     console.log(document.getElementById('edit-page-input-steps').value);
     console.log(document.getElementById('edit-page-input-notes').value);
   });
-  
 };
