76375ee637ae3785a85c9db31a39407b6a03af95
Kelin Lyu <KelinLyuACWG@gmail.com>
2022-11-17 10:45:25 -0800

    Link the script and update the main body part.

--- source/js/edit-page.js
+++ source/js/edit-page.js
@@ -1,11 +1,27 @@
 /**
- * Logs hello world to the console
+ * construct the edit page based on whether the user
+ * is creating a new recipe or editing a current one.
  *
  */
-function hello() {
-  console.log('hello world');
+function construct_edit_page() {
+  console.log('constructing the edit page...');
 }
 
-hello();
+window.onload = construct_edit_page;
 
+/**
+ * return to the previous page.
+ *
+ */
+function edit_page_back_event_handler() {
+  window.alert('Test: returning to the previous page...');
+}
 
+/**
+ * save the current data by creating a new element
+ * or overwrite the existing one.
+ *
+ */
+ function edit_page_save_event_handler() {
+  window.alert('Test: saving the element...');
+}
