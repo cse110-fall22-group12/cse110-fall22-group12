387a61754e376749f04ef57b787d93808201ac65
Kristy <k4chan@ucsd.edu>
2022-11-23 00:08:22 -0800

    view-page-add-delete-box

--- source/js/view.js
+++ source/js/view.js
@@ -5,16 +5,28 @@ function construct_view_page() {
   document.getElementById("recipe-preparation").innerHTML = data.preparation;
   document.getElementById("recipe-notes").innerHTML = data.notes;
 
-  document.getElementById("view-page-add-button").addEventListener('click', function(event) {
+  document.getElementById("view-page-home-button").addEventListener('click', function(event) {
+    window.location.href = "home.html";
+  });
+
+  document.getElementById("view-page-edit-button").addEventListener('click', function(event) {
     window.location.href = "edit-page.html";
   });
 
   document.getElementById("view-page-delete-button").addEventListener('click', function(event) {
+    let deleteElement = document.getElementById("delete-button-box");
+    deleteElement.style.display = "block";
+  });
+
+  document.getElementById("yes-delete-button").addEventListener('click', function(event) {
     delete_selected_data();
     window.location.href = "home.html";
   });
-
   
+  document.getElementById("no-delete-button").addEventListener('click', function(event) {
+    let deleteElement = document.getElementById("delete-button-box");
+    deleteElement.style.display = "none";
+  });
 }
 
 window.onload = construct_view_page;
