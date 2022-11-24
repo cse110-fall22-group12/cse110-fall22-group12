b104febad0bfb7d55011c49b41c95fdb28900df8
TomlandHilarious <91652058+TomlandHilarious@users.noreply.github.com>
2022-11-23 18:33:57 -0800

    fix bug on view page

--- source/js/view.js
+++ source/js/view.js
@@ -1,5 +1,5 @@
 /**
- * set up all the functilnalities for home button, edit button and 
+ * set up all the functilnalities for home button, edit button and
  * delete button.
  */
 function construct_view_page() {
@@ -10,34 +10,34 @@ function construct_view_page() {
   // document.getElementById('recipe-notes').innerHTML = data.notes;
 
   // when home button is clicked,  transit to home page
-  document.getElementById('view-page-home-button').addEventListener('click', 
-    function(event) {
-    window.location.href = 'home.html';
-  });
+  document.getElementById('view-page-home-button').addEventListener('click',
+      function(event) {
+        window.location.href = 'home.html';
+      });
   // when edit button is clicked, transit to edit page
-  document.getElementById('view-page-edit-button').addEventListener('click', 
-    function(event) {
-    window.location.href = 'edit.html';
-  });
+  document.getElementById('view-page-edit-button').addEventListener('click',
+      function(event) {
+        window.location.href = 'edit.html';
+      });
   // when delete button is clicked, the warning box pop out
-  document.getElementById('view-page-delete-button').addEventListener('click', 
-    function(event) {
-    const deleteElement = document.getElementById('delete-button-box');
-    deleteElement.style.display = 'block';
-  });
+  document.getElementById('view-page-delete-button').addEventListener('click',
+      function(event) {
+        const deleteElement = document.getElementById('delete-button-box');
+        deleteElement.style.display = 'block';
+      });
   // when delete pop-up box's yes button is clicked, transit to home page
-  document.getElementById('yes-delete-button').addEventListener('click', 
-  function(event) {
-    // delete_selected_data();
-    window.location.href = 'home.html';
-  });
+  document.getElementById('yes-delete-button').addEventListener('click',
+      function(event) {
+        // delete_selected_data();
+        window.location.href = 'home.html';
+      });
   // when delete pop-up box's no button is clicked, stay at the edit page
-  document.getElementById('no-delete-button').addEventListener('click', 
-    function(event) {
-    const deleteElement = document.getElementById('delete-button-box');
-    deleteElement.style.display = 'none';
-  });
+  document.getElementById('no-delete-button').addEventListener('click',
+      function(event) {
+        const deleteElement = document.getElementById('delete-button-box');
+        deleteElement.style.display = 'none';
+      });
 }
 
-//display the changes on view page
+// display the changes on view page
 window.onload = construct_view_page;
