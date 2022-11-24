8665cce0f7f363631d91a7b9969cdb96d3248fba
TomlandHilarious <91652058+TomlandHilarious@users.noreply.github.com>
2022-11-23 17:35:43 -0800

    Fixed error mentioend in last meeting.
    Co-authored-by: Kelin Lyu <KelinLyu@users.noreply.github.com>
    Co-authored-by: Yunyi She <yunyiwinnieshe@users.noreply.github.com>

--- source/js/view.js
+++ source/js/view.js
@@ -1,42 +1,33 @@
-/* global
-    get_selected_data,
-    delete_selected_data
-*/
-
 function construct_view_page() {
-  const data = get_selected_data();
-  document.getElementById('recipe-name').innerHTML = data.name;
-  document.getElementById('recipe-ingredients').innerHTML = data.ingredients;
-  document.getElementById('recipe-preparation').innerHTML = data.steps;
-  document.getElementById('recipe-notes').innerHTML = data.notes;
+  // const data = get_selected_data();
+  // document.getElementById('recipe-name').innerHTML = data.name;
+  // document.getElementById('recipe-ingredients').innerHTML = data.ingredients;
+  // document.getElementById('recipe-preparation').innerHTML = data.steps;
+  // document.getElementById('recipe-notes').innerHTML = data.notes;
 
-  document.getElementById('view-page-home-button').addEventListener('click',
-      function(event) {
-        window.location.href = 'home.html';
-      });
-
-  document.getElementById('view-page-edit-button').addEventListener('click',
-      function(event) {
-        window.location.href = 'edit.html';
-      });
-
-  document.getElementById('view-page-delete-button').addEventListener('click',
-      function(event) {
-        const deleteElement = document.getElementById('delete-button-box');
-        deleteElement.style.display = 'block';
-      });
-
-  document.getElementById('yes-delete-button').addEventListener('click',
-      function(event) {
-        delete_selected_data();
-        window.location.href = 'home.html';
-      });
-
-  document.getElementById('no-delete-button').addEventListener('click',
-      function(event) {
-        const deleteElement = document.getElementById('delete-button-box');
-        deleteElement.style.display = 'none';
-      });
+  // when home button is clicked,  transit to home page
+  document.getElementById('view-page-home-button').addEventListener('click', function(event) {
+    window.location.href = 'home.html';
+  });
+  // when edit button is clicked, transit to edit page
+  document.getElementById('view-page-edit-button').addEventListener('click', function(event) {
+    window.location.href = 'edit.html';
+  });
+  // when delete button is clicked, the warning box pop out
+  document.getElementById('view-page-delete-button').addEventListener('click', function(event) {
+    const deleteElement = document.getElementById('delete-button-box');
+    deleteElement.style.display = 'block';
+  });
+  // when delete pop-up box's yes button is clicked, transit to home page
+  document.getElementById('yes-delete-button').addEventListener('click', function(event) {
+    // delete_selected_data();
+    window.location.href = 'home.html';
+  });
+  // when delete pop-up box's no button is clicked, stay at the edit page
+  document.getElementById('no-delete-button').addEventListener('click', function(event) {
+    const deleteElement = document.getElementById('delete-button-box');
+    deleteElement.style.display = 'none';
+  });
 }
 
 window.onload = construct_view_page;
