aaa30d23ca5848da72330051caeb476fa856e7a2
l2lee <l2lee@ucsd.edu>
2022-11-29 14:30:27 -0800

    fix naming errors

--- source/js/test-jest.js
+++ source/js/test-jest.js
@@ -1,11 +1,11 @@
 module.exports = sum;
 /**
- * Add to numbers 
+ * Add to numbers
  * @param {number} a
  * @param {number} b
- * @returns {number} result of a + b 
+ * @returns {number} result of a + b
  */
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
