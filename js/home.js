43a7672e40703192d606737f2216733c1ad5334e
l2lee <62985418+l2lee@users.noreply.github.com>
2022-11-30 01:51:24 +0000

    Deploying to gh-pages from @ cse110-fall22-group12/cse110-fall22-group12@9dfee6970aa3fc63c652e7c22262ee1f1c29cfbd 🚀

---
+++ js/home.js
@@ -0,0 +1,64 @@
+/* global
+    is_launched_for_the_first_time,
+    select_data_by_id,
+    create_new_data,
+    new_data_index,
+    read_data_array
+*/
+window.addEventListener('DOMContentLoaded', init);
+
+/* Turn this off for deployment, but keep on for testing */
+const DEV = true;
+
+/**
+ * Initialization function that adds recipe cards from local storage to the html
+ * document. This function also has the ability to initialize our recipes to a
+ * specific set if we are doing development and/or manual testing.
+ *  */
+function init() {
+  if (is_launched_for_the_first_time() && DEV) {
+    create_new_data(
+        'mushroom killer',
+        ['tag', 'another tag', 'the last tag'],
+        true,
+        'some ingredients',
+        'some preparation',
+        'some notes',
+    );
+    create_new_data(
+        'mushroom terminator',
+        ['tag'],
+        true,
+        'some ingredients',
+        'some preparation',
+        'some notes',
+    );
+    create_new_data(
+        'mushroom slayer',
+        ['another tag', 'the last tag'],
+        false,
+        'some ingredients',
+        'some preparation',
+        'some notes',
+    );
+  }
+
+  const list = document.getElementsByClassName('recipe-list')[0];
+
+  const data_array = read_data_array('', false);
+  for (let i = 0; i < data_array.length; i += 1) {
+    const data = data_array[i];
+
+    // add our local storage keys as 'recipe cells' to the home page
+    const recipeCell = document.createElement('recipe-cell');
+    recipeCell.recipeData = data;
+    list.appendChild(recipeCell);
+  }
+
+  document.getElementById('button-container').addEventListener('click',
+      function(event) {
+        select_data_by_id(new_data_index);
+
+        window.location.href = 'edit.html';
+      });
+}
