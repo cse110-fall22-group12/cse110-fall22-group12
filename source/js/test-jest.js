e839b6ceda143027425384bfa1b89c4540d2dd2c
TomlandHilarious <91652058+TomlandHilarious@users.noreply.github.com>
2022-11-30 18:47:52 -0800

    attempt to restyle view files Kristy&Ruoxuan

--- source/js/test-jest.js
+++ source/js/test-jest.js
@@ -1,11 +1,14 @@
+/* global
+
+ */
 module.exports = sum;
 /**
  * Add to numbers
  * @param {number} a
  * @param {number} b
- * @returns {number} result of a + b
+ * @return {number} result of a + b
  */
-
 function sum(a, b) {
   return a + b;
 }
+sum();
